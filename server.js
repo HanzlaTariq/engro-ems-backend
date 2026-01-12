1ad31d6a73c5d28fd851aa579338f7342a15b948
Hanzla Tariq <thanzla2005@gmail.com>
2026-01-12 22:05:02 +0500

    commit 7

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ const app = express();
 
 
 
-app.use(cors(
-  {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],  
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  }
-));
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'https://engro-ems-frontend.vercel.app'||'http://localhost:5173',
+
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -39,17 +40,17 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: err.message });
 });
 // server.js ke START mein
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://engro-ems-frontend.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://engro-ems-frontend.vercel.app');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//   res.header('Access-Control-Allow-Credentials', 'true');
   
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-  next();
-});
+//   if (req.method === 'OPTIONS') {
+//     return res.status(200).end();
+//   }
+//   next();
+// });
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
